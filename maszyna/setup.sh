32ceca628a2152eecd35a4560c4540cf483b975a
Jakub Nowak <jakub.jakub.nowak@gmail.com>
2021-04-14 21:57:49 +0200

    [maszyna] Change default PORT

--- maszyna/setup.sh
+++ maszyna/setup.sh
@@ -3,7 +3,7 @@
 set -o errexit -o pipefail -o nounset
 
 
-declare -r PORT="${PORT-1338}"
+declare -r PORT="${PORT-1339}"
 
 echo >&2 "Listening on port ${PORT}..."
 exec socat tcp-l:"$PORT",bind=localhost,reuseaddr,fork exec:./main.py
