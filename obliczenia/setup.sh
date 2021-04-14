012f7a18ae74c638ff619b8be9c7383d910e5015
Jakub Nowak <jakub.jakub.nowak@gmail.com>
2021-04-14 17:15:56 +0200

    [obliczenia] Bind to localhost

--- obliczenia/setup.sh
+++ obliczenia/setup.sh
@@ -6,4 +6,4 @@ set -o errexit -o pipefail -o nounset
 declare -r PORT="${PORT-1337}"
 
 echo >&2 "Listening on port ${PORT}..."
-exec socat tcp-l:"$PORT",reuseaddr,fork exec:./main.py
+exec socat tcp-l:"$PORT",bind=localhost,reuseaddr,fork exec:./main.py
